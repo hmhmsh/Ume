564e4b1ceb183e13199056438e1b0641217e718c
shunkun <y.y.22.t.h.1@gmail.com>
2020-03-03 10:43:05 +0900

    initialize

---
+++ Ume/Ume.h
@@ -0,0 +1,19 @@
+//
+//  Ume.h
+//  Ume
+//
+//  Created by shunkun on 2020/03/02.
+//  Copyright © 2020 shukun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ume.
+FOUNDATION_EXPORT double UmeVersionNumber;
+
+//! Project version string for Ume.
+FOUNDATION_EXPORT const unsigned char UmeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ume/PublicHeader.h>
+
+
