22119cf82fe6abfb3ee0145cb82a7b0626fc6a66
shunkun <y.y.22.t.h.1@gmail.com>
2020-03-03 10:53:20 +0900

    create Ume

---
+++ Ume/Ume.swift
@@ -0,0 +1,13 @@
+//
+//  Ume.swift
+//  Ume
+//
+//  Created by shunkun on 2020/03/03.
+//  Copyright © 2020 shukun. All rights reserved.
+//
+
+import Foundation
+
+public struct Ume {
+	private init() {}
+}
